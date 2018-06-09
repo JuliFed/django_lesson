5e66d30ccb144d03aaca7585dbb91a55bdd30066
JuliFed <fedorchenkojuli@gmail.com>
2018-06-09 12:39:13 +0300

    Django test project

---
+++ myapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+import uuid
+
+
+class Task(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    text = models.CharField(max_length=50)
+    checked = models.BooleanField()
+    # unique=True
+    # null=True,
+    # blank=True,
+    # default=False,
+    # verbose_name="Check"
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        # unique_together = ['text', 'checked']
