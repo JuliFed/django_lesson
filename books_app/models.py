33f44f763c3485eea29302acb9d7a861d87cf677
JuliFed <fedorchenkojuli@gmail.com>
2018-06-13 19:53:00 +0300

    Changed models.Manager for Book

--- books_app/models.py
+++ books_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class BookManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(author__isnull=False)
+
+    def with_tags(self, tag):
+        return self.get_queryset().filter(tags__name=tag)
+
+
 class Author(models.Model):
     name = models.CharField(max_length=20)
 
@@ -13,6 +21,9 @@ class Book(models.Model):
     title = models.CharField(max_length=64)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     tags = models.ManyToManyField(Tag, related_name='books', through='BooksTags')
+    object = models.Manager()
+    with_author = BookManager()
+    # в выборке Book.with_author.filter()
 
 
 class BooksTags(models.Model):
